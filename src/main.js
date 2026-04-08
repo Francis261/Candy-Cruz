1c6e2a47ebce102787357c1c9bb3dce7c76b84a8
Francis <francis@vmi3081222.contaboserver.net>
2026-04-08 12:38:07 +0200

    Remove duplicate scene registration

--- src/main.js
+++ src/main.js
@@ -4,14 +4,5 @@ window.addEventListener('load', () => {
     const config = getGameConfig();
     const game = new Phaser.Game(config);
     
-    game.scene.add('BootScene', config.scene[0]);
-    game.scene.add('MainMenu', config.scene[1]);
-    game.scene.add('LevelSelect', config.scene[2]);
-    game.scene.add('GameScene', config.scene[3]);
-    game.scene.add('UIScene', config.scene[4]);
-    game.scene.add('PauseScene', config.scene[5]);
-    game.scene.add('GameOverScene', config.scene[6]);
-    game.scene.add('LevelCompleteScene', config.scene[7]);
-    
     console.log('Candy Cruz Match-3 Game Initialized');
 });
